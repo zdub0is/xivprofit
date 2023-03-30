418d35a24fd0748e518586754fe3225aa27dd49a
Zeke <az.duboiz@gmail.com>
2023-03-30 15:01:47 -0700

    fixing footer bg transparency

--- src/CustomFooter.js
+++ src/CustomFooter.js
@@ -1,6 +1,6 @@
 function CustomFooter() {
     return (
-        <div className="navbar fixed-bottom justify-content-center text-center border-top">
+        <div className="navbar fixed-bottom justify-content-center text-center border-top navbg">
           <a href="https://zdub0is.github.com">Github</a>&nbsp;|&nbsp;Made by Zeke D.
         </div>
       );
