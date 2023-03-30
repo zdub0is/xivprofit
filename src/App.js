b292681df879043eb9cfd64f1809d641df7984f2
Zeke <az.duboiz@gmail.com>
2023-03-30 14:46:42 -0700

    updated to using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import "./App.css";
 import RecipeDetails from "./RecipeDetails";
@@ -8,14 +8,14 @@ import Homepage from "./Homepage";
 
 function App() {
   return (
-    <Router basename="https://zdub0is.github.io/xivprofit">
+    <HashRouter>
       <Container className="white-text">
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </Container>
-    </Router>
+    </HashRouter>
   );
 }
 
